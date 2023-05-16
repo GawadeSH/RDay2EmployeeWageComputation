6f02c3d894e0339c70c711501cc7e1800d2375d0
shital gawade <shitalgawade131@gmail.com>
2023-05-16 14:06:46 +0530

    Attendance

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,10 +1,33 @@
 package com.employeewage;
 
-public class EmployeeWageComputation
-{
+import java.util.Random;
+
+public class EmployeeWageComputation {
+    int Attend1;
+
+    EmployeeWageComputation()
+    {
+        Random r = new Random();
+        Attend1 =r.nextInt(2);
+    }
+
+    void attendance()
+    {
+        if(Attend1==1)
+        {
+            System.out.println("Employee is present");
+        }
+        else if (Attend1==0)
+        {
+            System.out.println("Employee is absent");
+        }
+    }
     public static void main(String[] args)
     {
         System.out.println("Welcome to Employee Wage Computation Program");
+        EmployeeWageComputation E = new EmployeeWageComputation();
+        E.attendance();
+
 
     }
 }
