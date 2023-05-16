66a2e9feabafeb78cbd9acc9b8f6a2453fcf2ceb
shital gawade <shitalgawade131@gmail.com>
2023-05-16 14:11:21 +0530

    wage

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -2,13 +2,16 @@ package com.employeewage;
 
 import java.util.Random;
 
-public class EmployeeWageComputation {
-    int Attend1;
+public class EmployeeWageComputation
+{
+    int Attend1,Attend2;
+    int EmpRatePerHr,EmpHrs,w;
 
     EmployeeWageComputation()
     {
         Random r = new Random();
         Attend1 =r.nextInt(2);
+        Attend2 = r.nextInt(2);
     }
 
     void attendance()
@@ -22,11 +25,34 @@ public class EmployeeWageComputation {
             System.out.println("Employee is absent");
         }
     }
+    void wage()
+    {
+        if(Attend1==1 && Attend2==1)
+        {
+            EmpRatePerHr = 20;
+            EmpHrs = 8;
+            w = EmpRatePerHr * EmpHrs;
+            System.out.println("FullTime employee and Employee Wage is :-" + w);
+        }
+        else if (Attend1==1 && Attend2==0)
+        {
+            EmpRatePerHr = 20;
+            EmpHrs = 4;
+            w = EmpRatePerHr * EmpHrs;
+            System.out.println("PartTime employee and Employee Wage is :-" + w);
+        }
+        else if(Attend1==0)
+        {
+            System.out.println("Employee Wage is :-0");
+        }
+
+    }
     public static void main(String[] args)
     {
         System.out.println("Welcome to Employee Wage Computation Program");
         EmployeeWageComputation E = new EmployeeWageComputation();
         E.attendance();
+        E.wage();
 
 
     }
